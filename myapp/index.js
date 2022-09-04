713eaa6a3a458fa9d07bcced92b107ba2969dc72
RomaSamkov <Romansamkov.rs@gmail.com>
2022-09-04 21:16:31 +0300

    Update index.js

--- myapp/index.js
+++ myapp/index.js
@@ -1,17 +1,6 @@
 const express = require("express");
-const path = require("path");
+
 const app = express();
-
-app.use((req, res, next) => {
-  console.log("Наше промежуточное ПО");
-  next();
-});
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/contact/:id", (req, res) => {
-  res.send(`<h1>Contact</h1> Параметр: ${req.params.id}`);
-});
 
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
