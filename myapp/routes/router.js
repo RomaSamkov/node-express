c2ef46dc3c35963f73d41b1db7207a2dfd1b9f1d
RomaSamkov <Romansamkov.rs@gmail.com>
2022-09-04 23:26:17 +0300

    Update router.js

--- myapp/routes/router.js
+++ myapp/routes/router.js
@@ -1,9 +1,22 @@
 const { Router } = require("express");
+const fs = require("fs/promises");
+const path = require("path");
+
+const usersPath = path.join(__dirname, "/../db/users.json");
 
 const router = Router();
 
-router.get("/test", (req, res) => {
-  res.json({ message: "Hello friend" });
+const getUsersList = async () => {
+  return JSON.parse(await fs.readFile(usersPath));
+};
+
+router.get("/users", async (req, res) => {
+  try {
+    const users = await getUsersList();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
