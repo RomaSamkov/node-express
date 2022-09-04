20401857e7eea15e17eeb0c89733c939f611c20c
RomaSamkov <Romansamkov.rs@gmail.com>
2022-09-04 22:11:10 +0300

    add routes

--- myapp/index.js
+++ myapp/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const router = require("./routes/router");
 
 const app = express();
+
+app.use(router);
 
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
